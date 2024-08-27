799e87e5108691c807cf398123635cec468529b4
Harsh verma <hv72002@gmail.com>
2024-08-27 06:43:33 +0530

    autentication is set

--- carSellingMarket/src/Home.jsx
+++ carSellingMarket/src/Home.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Category from './components/Category'
+import MostSearchedCar from './components/MostSearchedCar'
+import InfoSection from './components/InfoSection'
+import Footer from './components/Footer'
 
-const Home = () => {
+function Home() {
   return (
     <div>
-      Home
+        {/* Header  */}
+        <Header/>
+        {/* Hero  */}
+        <Hero/>
+        {/* Category  */}
+        <Category/>
+        {/* Most Searched Car  */}
+        <MostSearchedCar/>
+        {/* InfoSection  */}
+        <InfoSection/>
+        {/* Footer  */}
+        <Footer/>
+
     </div>
   )
 }
